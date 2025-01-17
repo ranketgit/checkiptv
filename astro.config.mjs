dc49c9b124a790fb3b4d39b1ea4e4cd2813fef91
mezz01 <mezzatamine2002@gmail.com>
2025-01-17 12:47:20 +0100

    fixing things

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import { never } from 'astro/zod';
 
 // https://astro.build/config
 export default defineConfig({
+  trailingSlash: "never",
   site: "https://checkedtv.com",
   integrations: [react(), tailwind(), sitemap()],
-
-
 });
